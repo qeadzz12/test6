adcfc30306f52dfc3ee8342013fc72016d40c522
김소예 <skoiyme@gmail.com>
2026-08-19 14:34:57 +0900

    첫 번째 Java 파일 생성

---
+++ src/test/JavaTest.java
@@ -0,0 +1,14 @@
+package test;
+
+public class JavaTest {
+	
+	
+	int num = 0;
+	String nema = "쟝";
+	
+	public static void main(String[] args) {
+		
+        System.out.println("team1 작업");
+    }
+
+}
